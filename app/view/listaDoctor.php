5cb347c85c382ea40f38399e8b4eef3ed62feaee
Juan Ignacio Rodriguez <juanitandil2000@gmail.com>
2025-09-15 19:07:14 -0300

    Depuracion del codigo

--- app/view/listaDoctor.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function mostrarListaDoctores() {
-        foreach ($this->doctores as $doctor) {
-            echo "ID: " . $doctor->getId() . " - " . $doctor->getNombre() . " " . $doctor->getApellido() . " (" . $doctor->getEspecialidad() . ")\n";
-        }
-    }
-?>    
-    
